0ac2638f0e47a62f6807492164c925cf5ec1858a
zjy0516 <riverclouds.zhu@qq.com>
2026-03-24 15:31:07 +0000

    init

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -351,6 +351,7 @@ def fused_qk_norm_rope(
     cos_sin_cache: torch.Tensor,
     is_neox: bool,
     position_ids: torch.Tensor,
+    forced_token_heads_per_warp: int = -1,
 ) -> None:
     torch.ops._C.fused_qk_norm_rope(
         qkv,
@@ -364,6 +365,7 @@ def fused_qk_norm_rope(
         cos_sin_cache,
         is_neox,
         position_ids,
+        forced_token_heads_per_warp,
     )
 
 
